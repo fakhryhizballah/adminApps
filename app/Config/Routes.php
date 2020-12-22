fcaca8c349a7f772e8c4fc06342d7b0c29a92647
fakhryhizballah <falehry88@gmail.com>
2020-12-22 23:55:42 +0700

    route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,7 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
+$routes->get('/', 'Auth::index');
 $routes->get('/admin', 'Admin::index');
 $routes->get('/admdriver', 'Admin::admdriver');
 $routes->get('/ptcv', 'Admin::ptcv');
