63c51f37f7f40a74d4512f106cae29b13c080327
fakhryhizballah <falehry88@gmail.com>
2022-01-15 01:56:00 +0700

    add   withCredentials: true

--- public/js/Mesin_socket.js
+++ public/js/Mesin_socket.js
@@ -1,5 +1,7 @@
 // const socket = io("http://localhost:3000");
-const socket = io("https://socket.spairum.my.id:3000");
+const socket = io("https://socket.spairum.my.id:3000"{
+    withCredentials: true
+});
 socket.on("connect", () => {
     console.log(socket.id); // x8WIv7-mJelg7on_ALbx
     // socket.emit("hello", "world");
