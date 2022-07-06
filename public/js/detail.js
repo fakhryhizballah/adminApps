aa221cb7a0e7a914789d39bd8899597c120a9517
fakhryhizballah <falehry88@gmail.com>
2022-07-07 00:32:22 +0700

    add img

--- public/js/detail.js
+++ public/js/detail.js
@@ -10,52 +10,41 @@ function detail(id) {
       // console.log(data[0]['id']);
       var lokasi = data.lokasi;
       let foto = data.foto;
-      //   let link = "";
-      //   foto.forEach(getFoto);
-
-      // function getFoto(item, index) {
-      //     link += item['foto'];
-      //     document.getElementById("tes").innerHTML = "<img class='d-block w-100' src='item['foto']' alt='First slide'></img>";
-      // }
-
-      //   const getimg = document.querySelectorAll(".foto");
 
       //   var tesElement = document.getElementById("tes");
       var fotoElement = document.getElementById("foto");
 
-      //   function createImageNode(item, index) {
-      //     var img = document.createElement("img");
-      //     img.src = item["foto"];
-      //     return img;
-      //   }
-
       function createDivNode(item, index) {
         var div = document.createElement("div");
-        div.className = "carousel-item";
-        div.innerHTML =
-          "<img class='d-block w-100' src='" + item["foto"] + "'>";
+        var img = document.createElement("img");
+        console.log(index);
+        (index == 0) ? div.className = "carousel-item active" : div.className = "carousel-item";
+        // div.className = "carousel-item";
+        div.appendChild(img);
+        img.className = "d-block w-100";
+        img.src = item["foto"];
         return div;
       }
 
-      foto.forEach((div) => {
-        fotoElement.appendChild(createDivNode(div));
-        // fotoElement.appendChild(createImageNode(img));
+      foto.forEach((div, index) => {
+        fotoElement.appendChild(createDivNode(div, index));
       });
 
-      console.log(fotoElement);
-
-      //   foto.forEach((img) => {
-      //     fotoElement.appendChild(createImageNode(img));
-      //   });
-
-      //   document.getElementById("foto").innerHTML = link;
       document.getElementById("nama").innerHTML = lokasi.nama;
       document.getElementById("jenis").innerHTML = lokasi.jenis;
       document.getElementById("geo").innerHTML = lokasi.geo;
       document.getElementById("gmaps").innerHTML = lokasi.gmaps;
       document.getElementById("keterangan").innerHTML = lokasi.keterangan;
-
       $("#detailModal").modal("show");
+      $('#detailModal').on('hidden.bs.modal', function () {
+        console.log('close Modal');
+        var element = document.getElementById("foto");
+        while (element.hasChildNodes()) {
+          element.removeChild(element.firstChild);
+        }
+        // scanner.stop();
+      })
     },
+
   });
 }
